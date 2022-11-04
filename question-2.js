894c2eaace29b3363fb40be541c2235f6088339b
jkjode <104823459+jkjode@users.noreply.github.com>
2022-11-04 12:47:56 +0100

    Add files via upload

---
+++ question-2.js
@@ -0,0 +1,42 @@
+const apiKey = "caee7fe086e34c50b7ad0a28603fa3a6";
+const url =
+  "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=";
+const gamesContainer = document.querySelector(".games");
+const loading = document.querySelector(".loading");
+
+async function getGames() {
+  try {
+    debugger;
+
+    const response = await fetch(url + apiKey);
+
+    loading.innerHTML = `<img src="./image/Bean Eater-1s-200px.gif"></img>`;
+
+    const data = await response.json();
+
+    const dataResponse = data.results;
+
+    return dataResponse;
+  } catch (error) {
+    console.log("Error");
+  }
+}
+
+async function listOfGames() {
+  getGames()
+    .then((data) => {
+      loading.innerHTML = `<img></img>`;
+      return data;
+    })
+    .then((data) => {
+      for (let i = 0; i < 8; i++) {
+        const gameName = data[i].name;
+        const gameRating = data[i].rating;
+        const gameTagsLength = data[i].tags.length;
+
+        gamesContainer.innerHTML += `<h2>${gameName}</h2><h3>${gameRating}</h3><h3>${gameTagsLength}</h3>`;
+      }
+    });
+}
+
+listOfGames();
