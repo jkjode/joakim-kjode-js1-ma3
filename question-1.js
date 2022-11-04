e1e285faaf5364ce4c24976c7a6c11d8a5a4e47b
jkjode <104823459+jkjode@users.noreply.github.com>
2022-11-04 14:00:29 +0100

    Finished MA3

--- question-1.js
+++
@@ -1,8 +0,0 @@
-//Q1 Convert the function below to an arrow function:
-// function getRemainder(a, b) {
-//   return a % b;
-// }
-
-const getRemainder = (a, b) => a % b;
-
-console.log(getRemainder(30, 50));
